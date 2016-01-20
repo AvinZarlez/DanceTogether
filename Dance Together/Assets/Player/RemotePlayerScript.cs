20baea8538016963135407d19b912fd6db2f732f
Tobiah Zarlez <Tobiah Zarlez>
2016-01-20 11:38:04 -0800

    Add code comment
    Former-commit-id: e2420c2af7e3eadae711228170b2cd534f782a0a

--- Dance Together/Assets/Player/RemotePlayerScript.cs
+++ Dance Together/Assets/Player/RemotePlayerScript.cs
@@ -39,7 +39,7 @@ public class RemotePlayerScript : MonoBehaviour {
         Vector3 goal = localPlayerStartingPosition;
         float degreeMath;
         if (numberOfPlayers > 2) degreeMath = position * (2 * Mathf.PI) / (numberOfPlayers - 1);
-        else degreeMath = Mathf.PI;
+        else degreeMath = Mathf.PI; //Cheat if 2 players. If theory this won't happen if only one player, but just in case also prevents divide by 0.
         goal.x += distance * Mathf.Cos(degreeMath);
         goal.y += distance * Mathf.Sin(degreeMath);
         transform.position = Vector3.MoveTowards(transform.position, goal, step);
