4aaebf508978c9194340940bdb908f231fdb4892
Tobiah Zarlez <Tobiah Zarlez>
2016-02-16 15:42:42 -0800

    Adjust camera based on distance.
    Former-commit-id: 417e3d4a1bd3f3557889a56288472a826f1aaf17

---
+++ Dance Together/Assets/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public float screenWidth = 8.0f;  // TO DO: Correct size? 
+
+    public Camera cam;
+
+    void Start()
+    {
+
+        screenWidth = screenWidth / Screen.height * Screen.width;
+        cam.orthographicSize = screenWidth * Screen.height / Screen.width;
+    }
+}
