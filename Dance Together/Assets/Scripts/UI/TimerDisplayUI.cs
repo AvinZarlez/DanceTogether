c680914d75babd5adbf85f176419392494a6444d
Brandon Sweatman <BrandonSweatman@gmail.com>
2020-04-07 16:27:12 -0700

    Timer display script
    A simple monobehaviour to display a value on update.

---
+++ Dance Together/Assets/Scripts/UI/TimerDisplayUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace App.UI
+{
+    public class TimerDisplayUI : MonoBehaviour
+    {
+        public bool ShowAsWholeNumber = true;
+        [SerializeField]
+        private TextMeshProUGUI numberTextField;
+
+        public void UpdateNumber(float _value)
+        {
+            if (ShowAsWholeNumber)
+            {
+                numberTextField.text = Mathf.CeilToInt(_value).ToString();
+            }
+            else
+            {
+                numberTextField.text = _value.ToString();
+            }
+        }
+    }
+}
