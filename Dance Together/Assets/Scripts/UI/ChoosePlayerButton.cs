402f030cf43095812a38b4393a199bd1621266b5
Brandon Sweatman <BrandonSweatman@gmail.com>
2020-04-07 16:10:47 -0700

    Adding EndGameManager
    EndGameManager is responsible for handling Actions once the game ends. A list of the participating player is populated and allows for a choice.
    
    supporting files:
    
    - ChoosePlayerButton : a button that displays a players color and number, and will allow selection of said data upon method "ButtonClicked()".
    
    - PlayerSnapShot : Is a data class that is constructed in the DanceTogetherGameManager, to save player info incase a player drops offline while the game is running. this snapshot can be referenced to complete the game incase the actual player becomes null.

---
+++ Dance Together/Assets/Scripts/UI/ChoosePlayerButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using App.Controllers;
+using App.Data;
+
+[RequireComponent(typeof(Button))]
+public class ChoosePlayerButton : MonoBehaviour
+{
+    EndGameManager controller;
+
+    [SerializeField]
+    private TextMeshProUGUI Text; // use text to display player id on button.
+    public PlayerDataSnapShot player;
+
+    public void UpdateButtonInfo(EndGameManager _controller, PlayerDataSnapShot _player)
+    {
+        if (_player.PlayerID == -1  || _controller == null)
+            return;
+
+        controller = _controller; // save reference to controller.
+        player = _player; // save player reference
+
+        Text.text = player.PlayerID.ToString(); // display player id.
+
+        GetComponent<Image>().color = player.PlayerColor.Color; // set image of button to player color.
+    }
+
+    public void ButtonClick()
+    {
+        if (player.PlayerID == -1 || controller == null)
+            return;
+
+        controller.ChoosePlayer(player);
+    }
+}
---
+++ Dance Together/Assets/Scripts/UI/EndGameManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using App.Networking;
+using App.Data;
+
+
+namespace App.Controllers
+{
+    public class EndGameManager : MonoBehaviour
+    {
+        private DanceTogetherGameManager controller;
+
+        [Header("Button Prefab")]
+        [SerializeField]
+        private ChoosePlayerButton buttonPrefab;
+
+        [Header("Managed GUI Elements")]
+        [SerializeField]
+        private Image playerColor;
+        [SerializeField]
+        private TextMeshProUGUI playerID;
+        [SerializeField]
+        private Button continueButton;
+        [SerializeField]
+        private Image choosenColorImage;
+        [SerializeField]
+        private TextMeshProUGUI choosenPlayerID;
+        [Header("Player List references")]
+        [SerializeField]
+        private RectTransform playerList; // rect parent to put button in. If I Fits, I Sits.
+        [SerializeField]
+        private View PopupView; // popup view to open and close to show player selection options
+
+        private List<ChoosePlayerButton> playerButtons = new List<ChoosePlayerButton>();
+
+
+        public void Init(DanceTogetherGameManager _controller)
+        {
+            controller = _controller;
+
+            if(controller == null)
+            {
+                Debug.Log("Controller was assigned null.");
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Call Populate List from UI button.
+        /// </summary>
+        public void PopulateList()
+        {
+            ClearButtons();
+            foreach(PlayerDataSnapShot playerSS in controller.ActivePlayerData)
+            {
+                // dont add ourselves to the choosable list.
+                if (!playerSS.IsLocalPlayer)
+                {
+                    ChoosePlayerButton newButton = Instantiate(buttonPrefab, playerList);
+                    newButton.UpdateButtonInfo(this, playerSS);
+                    playerButtons.Add(newButton);
+                }
+            }
+
+            PopupView.OpenView();
+        }
+
+        public void ClearButtons()
+        {
+            foreach (ChoosePlayerButton button in playerButtons)
+            {
+                Destroy(button.gameObject);
+            }
+            playerButtons.Clear();
+        }
+
+        /// <summary>
+        /// Call continue from UI button
+        /// </summary>
+        public void Continue()
+        {
+            controller.LocalGotoPostGame();
+        }
+
+        public void ChoosePlayer(PlayerDataSnapShot player)
+        {
+            // set local view
+            //Debug.Log("local player ? : " + controller.LocalPlayer.PlayerID);
+            controller.LocalPlayer.CmdSetSongMatchID(player.SongID);
+            continueButton.interactable = true;
+            // close popup
+            PopupView.CloseView();
+            // set choose player button to match selection
+            choosenColorImage.color = player.PlayerColor.Color;
+            choosenPlayerID.text = player.PlayerID.ToString();
+        }
+
+        private void OnEnable()
+        {
+            if(controller.LocalPlayer == null)
+            {
+                Debug.LogWarning("Local Player is null");
+                return;
+            }
+            playerColor.color = controller.LocalPlayer.playerColor.Color;
+            playerID.text = controller.LocalPlayer.PlayerID.ToString();
+            if (controller.ActivePlayerData.Count > 1)
+                continueButton.interactable = false; // default behaviour.
+            else
+                continueButton.interactable = true; // for testing app with single player.
+
+            // reset the choose player button
+            choosenColorImage.color = Color.white;
+            choosenPlayerID.text = "Tap to Choose Player";
+        }
+    }
+}
+
