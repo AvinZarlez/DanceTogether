5d75ff357c4297ca6489c5a65b9b622b194bb0e6
Brandon Sweatman <BrandonSweatman@gmail.com>
2020-04-07 16:37:04 -0700

    Audio Scriptable Objects
    - SongGenre : used to create a new Genre of music.
    
    a new Genre can be create via :
    Assets/Create/DanceTogether/Audio/Genre
    
    - MusicTrack : a file used to track an audio file, description and genre.
    
    a new MusicTrack can be created via:
    Assets/Create/DanceTogether/Audio/MusciTrack
    
    - TrackList: used to store a list of MusicTracks, a description, and a genre
    
    a new TrackList can be created via:
    Assets/Create/DanceTogether/Audio/TrackList

---
+++ Dance Together/Assets/Scripts/ScriptableObjects/TrackList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Audio
+{
+    /// <summary>
+    /// A list that is meant to be used to group MusicTracks together, primarily by genre.
+    /// </summary>
+    [CreateAssetMenu(menuName = "DanceTogether/Audio/TrackList", fileName = "New TrackList")]
+    public class TrackList : ScriptableObject
+    {
+        [SerializeField, TextArea]
+        private string description = "A brief description of the track list.";
+        [SerializeField]
+        private SongGenre genre;
+        [SerializeField]
+        private List<MusicTrack> musicTrackList;
+
+        public string Description
+        {
+            get { return description; }
+        }
+        public SongGenre Genre
+        {
+            get { return genre; }
+        }
+        public List<MusicTrack> MusicTrackList
+        {
+            get { return musicTrackList; }
+        }
+    }
+}
