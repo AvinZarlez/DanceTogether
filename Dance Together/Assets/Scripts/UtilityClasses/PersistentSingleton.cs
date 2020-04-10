c27bd693affbc3f9bbd519f42552341397166065
Brandon Sweatman <BrandonSweatman@gmail.com>
2020-04-10 16:42:21 -0700

    New Classes
    Added Singleton
    Added PersistentSingleton
    
    To be used as base classes for controllers.

---
+++ Dance Together/Assets/Scripts/UtilityClasses/PersistentSingleton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Utility
+{
+    /// <summary>
+    /// Singleton that persists across multiple scenes
+    /// </summary>
+    public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
