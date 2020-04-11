5c1493ec7073c903ffd6910b4d811196ce92d45a
Brandon Sweatman <BrandonSweatman@gmail.com>
2020-04-11 03:21:01 -0700

    Message View Script
    Add new script

---
+++ Dance Together/Assets/Scripts/UI/MessageView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(View))]
+public class MessageView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI TextDisplay;
+    
+    /// <summary>
+    /// Method to simply change the text message displayed.
+    /// </summary>
+    /// <param name="_message"></param>
+    public void UpdateMessage(string _message)
+    {
+        TextDisplay.text = _message;
+    }
+}
