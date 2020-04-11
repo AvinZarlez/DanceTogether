17d449a90a54fe9808a6186baf0adf28e0c23dbc
Brandon Sweatman <BrandonSweatman@gmail.com>
2020-04-11 03:22:02 -0700

    Added Editor Script
    This editor script is to allow manual testing of GameEvents in PlayMode from the editor.

---
+++ Dance Together/Assets/Editor/GameEventEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace App.Events
+{
+    [CustomEditor(typeof(GameEvent))]
+    public class GameEventEditor : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            GameEvent _GE = target as GameEvent;
+
+            if (GUILayout.Button("Raise"))
+            {
+                _GE.Raise();
+            }
+        }
+    }
+}
